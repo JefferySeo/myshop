bedf3620dcc75564a53238330b7076dedb58484a
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-03-07 18:01:45 +0900

    0307

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,25 @@
 (function(){
     const listAll = document.getElementsByClassName('list-all')[0];
+    const cart = document.getElementById('cart');
     listAll.addEventListener("click", changeNav);
+    cart.addEventListener('click', cartBoxView);
 
     function changeNav(){
         const nav = document.getElementsByTagName('nav')[0].offsetTop + 52;
-        document.getElementById('sitemap').style.top = nav + 'px';
-        document.getElementById('sitemap').style.display='block';
+        const sitemap = document.getElementById('sitemap');
         listAll.classList.toggle('close');
         listAll.classList.toggle('newlist');
+
+        if(listAll.className == 'list-all close'){
+            sitemap.style.top = nav + 'px';
+            sitemap.style.display='block';
+        }else{
+            sitemap.style.display='none';
+        }
+    }
+
+    function cartBoxView(){
+        document.getElementsByClassName('cart-view')[0].classList.toggle('none');
     }
 
     // window.onload = function(){
