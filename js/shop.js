5dc1345a8c970758fb7f5291c1ed419cfe4af84a
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-03-07 16:52:08 +0900

    ㅅㅂ

--- js/shop.js
+++ js/shop.js
@@ -1,11 +1,15 @@
-$(function(){
-    $('.menu').mouseover(function(){
-        $(this).html('<span class="material-icons">more_horiz</span>');
-    });
-    $('.menu').mouseout(function(){
-        $(this).html('<span class="material-icons">menu</span>');
-    });
-    $('.menu').stop().click(function(){
-        $(this).html('<span class="material-icons">close</span>');
-    });
-})
+(function(){
+    const listAll = document.getElementsByClassName('list-all')[0];
+    listAll.addEventListener("click", changeNav);
+
+    function changeNav(){
+        const nav = document.getElementsByTagName('nav')[0].offsetTop + 52;
+        document.getElementById('sitemap').style.top = nav + 'px';
+        document.getElementById('sitemap').style.display='block';
+        listAll.classList.toggle('close');
+        listAll.classList.toggle('newlist');
+    }
+
+    // window.onload = function(){
+    // }
+}());
